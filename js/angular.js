a0b2199118f2f35612fcba612a19d94087dfadc4
galdinorosas <dino.rosas@gmail.com>
2016-02-28 02:23:04 -0800

    added in http get request to flickr, this returned an array[100] with image info.

--- js/angular.js
+++ js/angular.js
@@ -1,1 +1,36 @@
+angular.module('myApp', [])
+    .controller('myCtrl', ['$scope', '$http', function($scope, $http) {
 
+        $scope.introSearchSubmit = false;
+
+        $scope.introSearchTerm = "";
+        $scope.searchResults = {};
+
+        $scope.introSubmit = function(searchTerm) {
+            $scope.introSearchTerm = "";
+
+            var params = {
+                method: 'flickr.photos.search',
+                api_key: '080047a6f8b1c7aba461cb0f76ec0796',
+                tags: searchTerm,
+                format: 'json',
+                nojsoncallback: 1
+            };
+
+            $http({
+                    method: "GET",
+                    url: "https://api.flickr.com/services/rest",
+                    params: params
+                })
+                .then(function(response) {
+                        console.log();
+                        $scope.searchResults = response;
+                        console.log($scope.searchResults);
+
+                    },
+                    function(error) {
+
+                        console.log('error', error);
+                    });
+        };
+    }]);
