daf9d880d6e04e243364e0085182e149ee520c12
galdinorosas <dino.rosas@gmail.com>
2016-02-28 04:35:48 -0800

    added images from the user search. This required the ng-src directive. Also changed css to follow app requirements from Thinkful.

--- js/angular.js
+++ js/angular.js
@@ -1,36 +1,54 @@
 angular.module('myApp', [])
     .controller('myCtrl', ['$scope', '$http', function($scope, $http) {
 
-        $scope.introSearchSubmit = false;
+        $scope.introSearchSubmit = true;
 
         $scope.introSearchTerm = "";
-        $scope.searchResults = {};
+        $scope.currentSearchTerm = "";
+        $scope.searchResults = [];
+        $scope.initialBottomBar = { "margin-top": "23em" };
 
         $scope.introSubmit = function(searchTerm) {
+            $scope.currentSearchTerm = $scope.introSearchTerm;
             $scope.introSearchTerm = "";
 
-            var params = {
-                method: 'flickr.photos.search',
-                api_key: '080047a6f8b1c7aba461cb0f76ec0796',
-                tags: searchTerm,
-                format: 'json',
-                nojsoncallback: 1
-            };
+            if ($scope.introForm.$valid) {
+                console.log('The form is valid');
+                console.log($scope.introSearchSubmit);
 
-            $http({
-                    method: "GET",
-                    url: "https://api.flickr.com/services/rest",
-                    params: params
-                })
-                .then(function(response) {
-                        console.log();
-                        $scope.searchResults = response;
-                        console.log($scope.searchResults);
+                var params = {
+                    method: 'flickr.photos.search',
+                    api_key: '080047a6f8b1c7aba461cb0f76ec0796',
+                    tags: searchTerm,
+                    format: 'json',
+                    nojsoncallback: 1
+                };
 
-                    },
-                    function(error) {
+                $http({
+                        method: "GET",
+                        url: "https://api.flickr.com/services/rest",
+                        params: params
+                    })
+                    .then(function(response) {
+                            console.log();
+                            $scope.searchResults = response.data.photos.photo;
+                            console.log($scope.searchResults);
+                            $scope.introSearchSubmit = false;
+                            $scope.initialBottomBar = { "margin-top": "13em" };
 
-                        console.log('error', error);
-                    });
+                        },
+                        function(error) {
+
+                            console.log('error', error);
+                        });
+
+            } else {
+                console.log('The form is invalid');
+            }
+
+
         };
+
+        console.log($scope.introSearchSubmit);
+
     }]);
